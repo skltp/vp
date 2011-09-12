6af85ec56ac563ebf2883f3446408f138158be17
matsek <matsek@c1b456d0-7b02-511b-e5bf-04bacca435da>
2011-09-12 13:16:23 +0000

    Changed exception logic to match Mule version 2.2.8

--- vp-services/src/main/java/se/skl/tp/vp/vagvalrouter/ExceptionTransformer.java
+++ vp-services/src/main/java/se/skl/tp/vp/vagvalrouter/ExceptionTransformer.java
@@ -20,25 +20,14 @@
  */
 package se.skl.tp.vp.vagvalrouter;
 
-import java.io.StringWriter;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.apache.cxf.binding.soap.SoapFault;
+import org.apache.commons.httpclient.HttpException;
 import org.mule.api.MuleMessage;
 import org.mule.api.routing.RoutingException;
+import org.mule.api.service.ServiceException;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageAwareTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 import se.skl.tp.vp.exceptions.VpSemanticException;
 import se.skl.tp.vp.exceptions.VpTechnicalException;
@@ -56,58 +45,48 @@ public class ExceptionTransformer extends AbstractMessageAwareTransformer {
 	public Object transform(MuleMessage msg, String encoding) throws TransformerException {
 		// Check if any error
 		if (msg.getExceptionPayload() != null) {
-			logger.debug("Exceptionpayload detected!");
-			String exceptionMessage = "";
-			if (msg.getExceptionPayload().getException() instanceof RoutingException) {
-				// Check what caused the routing exception
-				RoutingException routingException = (RoutingException) msg.getExceptionPayload().getException();
-				Throwable cause = routingException.getCause();
-				if (cause != null) {
-					// Check for defined TP exceptions
-					if (cause instanceof VpSemanticException || cause instanceof VpTechnicalException) {
-						return createSoapFault(msg, cause.getMessage(), null);
-					} else {						
-						Throwable nestedCause = cause;
-						exceptionMessage = nestedCause.getMessage();
-						// Just to be safe and avoid unlimited loops
-						int i = 1;
-						while (nestedCause.getCause() != null && i < 10){
-							nestedCause = nestedCause.getCause();
-							exceptionMessage = nestedCause.getMessage();
-							
-							// Try to see if we got a SoapFault response. If so we should recreate this and send back!
-							if (nestedCause instanceof SoapFault) {
-								logger.debug("SoapFault detected!");
-								return createSoapFault(msg, "", (SoapFault)nestedCause);
-							}
-							i++;
-						}						
-					} 
+			logger.debug("Exception payload detected!");
+			if (msg.getExceptionPayload().getException() instanceof ServiceException ||
+				msg.getExceptionPayload().getException() instanceof HttpException) {
+				
+				// Check for defined TP exceptions
+				if (msg.getExceptionPayload().getException() instanceof ServiceException &&
+					msg.getExceptionPayload().getException().getCause() != null ) {
+					if (msg.getExceptionPayload().getException().getCause() instanceof VpSemanticException || 
+						msg.getExceptionPayload().getException().getCause() instanceof VpTechnicalException) {
+						return createSoapFault(msg, msg.getExceptionPayload().getException().getCause().getMessage());
+					}					
 				}
+				// Check if we got any payload if so return it!
+				if (msg.getPayload() instanceof org.mule.transport.NullPayload) {
+					logger.debug("Nullpayload detected!");
+					return createSoapFault(msg, "VP009 Exception when calling the service producer, connection closed");					
+				} else {
+					logger.debug("Payload detected!");
+					msg.setExceptionPayload(null);
+					return msg.getPayload();	
+				}								
+			} else if (msg.getExceptionPayload().getException() instanceof RoutingException) {
+				// Here we could get some data in payload but don't use it!
+				logger.debug("Routingexception detected!");
+				return createSoapFault(msg, "VP009 Exception when calling the service producer!");					
 			}
-			// No soapFault or TP exception found, returned found exception
-			return createSoapFault(msg, "VP009 Exception when calling the service producer:" + exceptionMessage, null);
+			// No defined exception above or TP exception found
+			return createSoapFault(msg, "VP009 Exception when calling the service producer!");
 		} else if (msg.getPayload() instanceof org.mule.transport.NullPayload) {
-			logger.debug("Nullpayload detected!");
-			return createSoapFault(msg, "VP009 Exception when calling the service producer, connection closed", null);
+			// No exception pauload and no message payload
+			logger.debug("Nullpayload detected in message!");
+			return createSoapFault(msg, "VP009 Exception when calling the service producer, connection closed");
 		}
 		
 		// No error, return incoming payload!
 		return msg.getPayload();
 	}
 
-	private Object createSoapFault(MuleMessage msg, String srcCause, SoapFault originalSoapFault) {
+	private Object createSoapFault(MuleMessage msg, String srcCause) {
 		StringBuffer result = new StringBuffer();
 		
-		// Check if to recreate a SoapFault or create a new
-		if (originalSoapFault != null) {
-			String faultCode = "soap:" + originalSoapFault.getFaultCode().getLocalPart();
-			String faultString = originalSoapFault.getMessage();
-			String faultDetail = getFaultDetail(originalSoapFault);
-			reCreateSoapFault(result, faultCode, faultString, faultDetail);
-		} else {
-			createSoapFault(result, srcCause);
-		}
+		createSoapFault(result, srcCause);
 				
 		// Now wrap it into a soap-envelope
 		result = createSoapEnvelope(result);
@@ -123,52 +102,6 @@ public class ExceptionTransformer extends AbstractMessageAwareTransformer {
 		
 		// Done, return the string
 		return result.toString();
-	}
-
-	private String getFaultDetail(SoapFault soapFault) {
-		StringBuffer soapDetail = new StringBuffer();
-		if (soapFault.hasDetails()) {
-			logger.debug("getFaultDetail: Detail found.");
-			Element detail = soapFault.getDetail();
-			// Get xml string by applying fake XSLT transform of detail element
-			try {
-				// XSLT
-				StringWriter xmlStringWriter = new StringWriter();
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-				DocumentBuilder db = dbf.newDocumentBuilder();
-				Document dom = db.newDocument();	
-				Element root = dom.createElement("root");
-	            dom.appendChild(root);  	            
-	            Node detailNode = dom.importNode(detail,true); //true if you want a deep copy	  
-	            Element root2 = dom.getDocumentElement();
-	            root2.appendChild(detailNode);		
-				DOMSource domSource = new DOMSource(dom);
-				StreamResult streamResult = new StreamResult(xmlStringWriter);
-				TransformerFactory tf = TransformerFactory.newInstance();
-				Transformer serializerXSLT = tf.newTransformer();
-				serializerXSLT.setOutputProperty(javax.xml.transform.OutputKeys.OMIT_XML_DECLARATION, "yes");
-				serializerXSLT.transform(domSource, streamResult);		
-				// Strip off <root>  and </root> element from string
-				soapDetail.append(xmlStringWriter.toString().substring(6, xmlStringWriter.toString().length()-7));
-				logger.debug("XSLT transform:" + soapDetail);
-			} catch(Exception ie) {
-				logger.error("XSLT transform exception:" + ie.getMessage());
-			}
-			return soapDetail.toString();			
-		} else {
-			logger.debug("getFaultDetail: No detail found.");
-			return "";
-		}
-	}
-	
-	private void reCreateSoapFault(StringBuffer result, String faultCode, String faultString, String faultDetail) {
-		result.append("<soap:Fault>");
-		result.append("<faultcode>" + faultCode + "</faultcode>");
-		result.append("<faultstring>" + faultString + "</faultstring>");
-		if (faultDetail != null && faultDetail.length() > 0) {
-			result.append(faultDetail);
-		}
-		result.append("</soap:Fault>");
 	}
 
 	private void createSoapFault(StringBuffer result, String faultString) {
