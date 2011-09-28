9f1deccb79601e549eabb130d7c3eb51c70a1027
marcuskrantz <marcuskrantz@c1b456d0-7b02-511b-e5bf-04bacca435da>
2011-09-28 09:37:47 +0000

    * Added install instructions for 1.3.1 * Fixed bug in 1.3.1 sql upgrade script

--- vp-distribution/src/resources/sql/tp_logs_1.3.1.sql
+++ vp-distribution/src/resources/sql/tp_logs_1.3.1.sql
@@ -1,6 +1,6 @@
 use tp_logs;
 
 alter table session 
-	add column status varchar(16) unsigned default '',
+	add column status varchar(16) default '',
 	add column error_description varchar(255) default '',
 	add column technical_error_description varchar(255) default '';
