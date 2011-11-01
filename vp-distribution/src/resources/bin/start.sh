53320ad07bc7bf3b181aadeee276d32f3a0cc3e5
peterlarsson <peterlarsson@c1b456d0-7b02-511b-e5bf-04bacca435da>
2011-11-01 12:03:05 +0000

    Updates after scratch install on clean machine.
    - Installation documentation is now in PA2
    - Use environment variables in properties files
    - Set environment variables in start/stop scripts
    - Moved jmx.password to mule_home/conf
    - Removed old environment variables files

--- vp-distribution/src/resources/bin/start.sh
+++ vp-distribution/src/resources/bin/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -n "Starting ActiveMQ 5.4.2..."
-cd ~/vp/apache-activemq-5.4.2/bin
+d=`dirname $0`/..
+export VP_HOME=`pwd $d`
+export MULE_HOME=${VP_HOME}/mule-standalone-2.2.8
+
+echo -n "Starting ActiveMQ 5.4.3..."
+cd ${VP_HOME}/apache-activemq-5.4.3/bin
 ./activemq start &>/dev/null
 
 if [ $? == 0 ]; then
@@ -9,7 +13,7 @@ if [ $? == 0 ]; then
 fi
 
 echo -n "Starting Mule 2.2.8..."
-cd ~/vp/mule-standalone-2.2.8/bin
+cd ${MULE_HOME}/bin
 ./mule start -config vp-config.xml 2>&1 >/dev/null
 
 if [ $? == 0 ]; then
