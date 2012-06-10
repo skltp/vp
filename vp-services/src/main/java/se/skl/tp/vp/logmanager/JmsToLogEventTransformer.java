213c8c1a792b4f70e704b1532b295e1279ef7683
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-06-10 18:00:45 +0000

    Made Jaxbcontext static for LogEvents, for performance issues because high GC

--- vp-services/src/main/java/se/skl/tp/vp/logmanager/JmsToLogEventTransformer.java
+++ vp-services/src/main/java/se/skl/tp/vp/logmanager/JmsToLogEventTransformer.java
@@ -32,26 +32,35 @@ import org.soitoolkit.commons.logentry.schema.v1.LogEvent;
 /**
  * 
  * @author Marcus Krantz [marcus.krantz@callistaenterprise.se]
- *
+ * 
  */
 public class JmsToLogEventTransformer extends AbstractTransformer {
 
 	private static final Logger log = LoggerFactory.getLogger(JmsToLogEventTransformer.class);
-	
+
+	private static final JAXBContext context = initContext();
+
+	private static JAXBContext initContext() {
+		try {
+			return JAXBContext.newInstance(LogEvent.class);
+		} catch (JAXBException e) {
+			throw new RuntimeException("Unable to create JAXBContext for LogEvent", e);
+		}
+	}
+
 	@Override
-	protected Object doTransform(Object arg0, String arg1)
-			throws TransformerException {
-		
+	protected Object doTransform(Object arg0, String arg1) throws TransformerException {
+
 		log.debug("Transforming {} to log event object", arg0.getClass().getName());
-		
+
 		if (arg0 instanceof ActiveMQTextMessage) {
-			
+
 			final ActiveMQTextMessage msg = (ActiveMQTextMessage) arg0;
-			
+
 			try {
-				final JAXBContext jc = JAXBContext.newInstance(LogEvent.class);
-				final LogEvent le = (LogEvent) jc.createUnmarshaller().unmarshal(new ByteArrayInputStream(msg.getText().getBytes()));
-				
+				final LogEvent le = (LogEvent) context.createUnmarshaller().unmarshal(
+						new ByteArrayInputStream(msg.getText().getBytes()));
+
 				return le;
 			} catch (JMSException e) {
 				throw new TransformerException(this, e);
@@ -59,8 +68,9 @@ public class JmsToLogEventTransformer extends AbstractTransformer {
 				throw new TransformerException(this, e);
 			}
 		}
-		
-		throw new TransformerException(this, new IllegalStateException("Object to transform is not an ActiveMQTextMessage but was: {}", arg0.getClass().getName()));
+
+		throw new TransformerException(this, new IllegalStateException(
+				"Object to transform is not an ActiveMQTextMessage but was: {}", arg0.getClass().getName()));
 	}
 
 }
