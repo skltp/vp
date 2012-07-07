44f6b41d3f7343d29f4448bd2472cdd0aae21caf
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-07-07 14:34:01 +0000

    auto deployer added

--- vp-distribution/src/resources/bin/start.sh
+++ vp-distribution/src/resources/bin/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 export VP_HOME="$( cd -P "$( dirname $0 )"/.. && pwd )"
-export MULE_HOME=${VP_HOME}/mule-standalone-2.2.8
+export MULE_HOME=${VP_HOME}/mule-standalone-3.3.0
+
+echo -n "Generates virtual service deployment descriptors..."
+$JAVA_HOME/bin/java -jar vp-auto-deployer-1.0.jar -out $MULE_HOME/lib/user/virtual-riv-services.jar -overwrite $VP_HOME/vp/services/*.jar
+if [ $? == 0 ]; then
+	echo "done."
+fi
 
 echo -n "Starting ActiveMQ 5.4.3..."
 cd ${VP_HOME}/apache-activemq-5.4.3/bin
@@ -11,7 +17,7 @@ if [ $? == 0 ]; then
 	echo "done."
 fi
 
-echo -n "Starting Mule 2.2.8..."
+echo -n "Starting Mule 3.3.0..."
 cd ${MULE_HOME}/bin
 ./mule start -config vp-config.xml 2>&1 >/dev/null
 
