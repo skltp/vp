3435d8020b83a99e5933744d5b7c6543b922cf73
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-07-08 13:49:08 +0000

    added copyright and license headers

--- vp-auto-deployer/src/test/java/se/skl/tp/vp/deployer/DeployerMainTest.java
+++ vp-auto-deployer/src/test/java/se/skl/tp/vp/deployer/DeployerMainTest.java
@@ -1,9 +1,32 @@
+/**
+ * Copyright (c) 2009-2012, Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
 package se.skl.tp.vp.deployer;
 
 import org.junit.Before;
 import org.junit.Test;
 
-//
+/**
+ * Simple tests.
+ * 
+ * @author Peter
+ */
 public class DeployerMainTest {
 
 	private DeployerMain dm;
@@ -14,11 +37,7 @@ public class DeployerMainTest {
 	}
 	
 	@Test
-	public void deploy() throws Exception {
+	public void updateDeploy() throws Exception {
 		dm.deploy("src/test/resources/test.jar", true);
-	}
-	
-	@Test
-	public void parseJar() throws Exception {
-	}
+	}	
 }
