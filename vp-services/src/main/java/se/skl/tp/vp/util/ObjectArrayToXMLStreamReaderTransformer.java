0c43bd759d7d3a2a37e92f65ac26d1e3f439d41b
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-07-20 11:58:54 +0000

    Improved and simplified payload handling.

--- vp-services/src/main/java/se/skl/tp/vp/util/ObjectArrayToXMLStreamReaderTransformer.java
+++ vp-services/src/main/java/se/skl/tp/vp/util/ObjectArrayToXMLStreamReaderTransformer.java
@@ -26,6 +26,7 @@ import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.mule.module.xml.transformer.XmlToXMLStreamReader;
 
 public class ObjectArrayToXMLStreamReaderTransformer extends AbstractTransformer {
 
