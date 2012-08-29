142b1a4d95a37b51a0c4a616331d9558d9780d3b
peter.larsson@callistaenterprise.se <peter.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-08-29 13:08:48 +0000

    Added instrumentation of connections and other important functionality. Total time, producer endpoint, routing , and loggig is instrumented.

---
+++ vp-services/src/main/java/se/skl/tp/vp/util/ExecutionTimer.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright 2012 Sjukvardsradgivningen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+
+ *   Boston, MA 02111-1307  USA
+ */
+package se.skl.tp.vp.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple utility to instrument different executions flows.
+ *
+ * @author Peter
+ * @since 2.0
+ */
+public class ExecutionTimer {
+	//
+	private static final Logger logger = LoggerFactory.getLogger(ExecutionTimer.class);
+	
+	private static ThreadLocal<Map<String, ExecutionTimer>> local = new ThreadLocal<Map<String, ExecutionTimer>>();
+	private String name;
+	private long start;
+	private long end;
+	
+	/**
+	 * Inits timer functionality.
+	 */
+	public static void init() {
+		logger.debug("init");
+		local.set(new HashMap<String, ExecutionTimer>());
+	}
+	
+	/**
+	 * Removes all timers.
+	 */
+	public static void remove() {
+		local.remove();
+	}
+	
+	/**
+	 * Returns the timer.
+	 * @param name the name.
+	 * @return the time by name, or null if not found.
+	 */
+	public static ExecutionTimer get(String name) {
+		Map<String, ExecutionTimer> map = getMap();
+		return (map == null) ? null : map.get(name);
+	}
+	
+	/**
+	 * Starts a timer with the given name.
+	 * 
+	 * @param name the name.
+	 */
+	public static void start(String name) {
+		logger.debug(name);
+		Map<String, ExecutionTimer> map = getMap();
+		if (map != null) {
+			map.put(name,  new ExecutionTimer(name));
+		}
+	}
+	
+	/**
+	 * Stops timer with name.
+	 * 
+	 * @param name the name of the timer.
+	 */
+	public static void stop(String name) {
+		logger.debug(name);
+		ExecutionTimer timer = get(name);
+		if (timer != null) {
+			timer.stop();
+			logger.debug(timer.toString());
+		}
+	}
+		
+	/**
+	 * Returns a formatted string with timers.
+	 */
+	public static String format() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("timers { ");
+		int len = buf.length();
+		Map<String, ExecutionTimer> map = getAll();
+		if (map != null) {
+			for (ExecutionTimer timer : map.values()) {
+				if (buf.length() > len) {
+					buf.append(", ");
+				}
+				buf.append(timer.toString());
+			}
+		}
+		buf.append(" }");
+		return buf.toString();
+	}
+	
+	/**
+	 * Returns all timers.
+	 * 
+	 * @return all timers, or null if none exists.
+	 */
+	public static Map<String, ExecutionTimer> getAll() {
+		return getMap();
+	}
+	
+	//
+	private ExecutionTimer(String name) {
+		this.name = name;
+		this.start = now();
+		this.end = -1L;
+	}
+	
+	//
+	private void stop() {
+		if (this.end == -1L) {
+			this.end = now();
+		}
+	}
+
+	/**
+	 * Returns the name.
+	 * 
+	 * @return the name.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Returns elapsed time in millis.
+	 * 
+	 * @return elapsed time in millis, or -1 if timer not has been stopped.
+	 */
+	public long getElapsed() {
+		return (end == -1L) ? -1L : (end - start);
+	}
+	
+	/**
+	 * Returns a string representation.
+	 * 
+	 * @return the string as <name> { elapsed: <elapsed> }
+	 */
+	public String toString() {
+		return String.format("%s: %d", getName(), getElapsed());
+	}
+
+	//
+	private static long now() {
+		return System.currentTimeMillis();
+	}
+	
+	//
+	private static Map<String, ExecutionTimer> getMap() {
+		return local.get();
+	}
+}
