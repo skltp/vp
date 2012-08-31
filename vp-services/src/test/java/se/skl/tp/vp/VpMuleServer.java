fa54bbb582a8fc2973732ba1e25bc5767cee8635
magnus.larsson@callistaenterprise.se <magnus.larsson@callistaenterprise.se@c1b456d0-7b02-511b-e5bf-04bacca435da>
2012-08-31 10:46:40 +0000

    Updates issue NTJP-113.
    1. Updated VPMuleServer.java:
      * Start mule flows and teststub-flows in a correct way
      * Initiate testdata for TK for the test-producer
    
    2. Added mandatory file mule-deploy.properties

--- vp-services/src/test/java/se/skl/tp/vp/VpMuleServer.java
+++ vp-services/src/test/java/se/skl/tp/vp/VpMuleServer.java
@@ -1,7 +1,13 @@
 package se.skl.tp.vp;
 
  
+import java.util.ArrayList;
+import java.util.List;
+
 import org.soitoolkit.commons.mule.test.StandaloneMuleServer;
+
+import se.skl.tp.vp.vagvalagent.SokVagvalsInfoMockInput;
+import se.skl.tp.vp.vagvalagent.VagvalMockInputRecord;
 
 
 public class VpMuleServer {
@@ -9,16 +15,37 @@ public class VpMuleServer {
 
 	public static final String MULE_SERVER_ID   = "vp";
  
-//	public static final String MULE_CONFIG      = "vp-teststubs-and-services-config.xml"; // both teststubs and services
-	public static final String MULE_CONFIG      = "vp-teststubs-only-config.xml"; // only teststubs
+	public static final String MULE_CONFIG      = "vp-teststubs-and-services-config.xml"; // both teststubs and services
+//	public static final String MULE_CONFIG      = "vp-teststubs-only-config.xml"; // only teststubs
 //	public static final String MULE_CONFIG      = "vp-config.xml"; // only services
 
 	public static void main(String[] args) throws Exception {
  
-		StandaloneMuleServer muleServer = new StandaloneMuleServer(MULE_SERVER_ID, MULE_CONFIG, false);
+		initTk();
+		
+		StandaloneMuleServer muleServer = new StandaloneMuleServer(MULE_SERVER_ID, MULE_CONFIG, true);
  
 		muleServer.run();
 	}
 	
- 
+	static private void initTk() {
+		// NOTE this test user the same certificates for consumer,
+		// virtualisation-plattform and producer
+		// The certs are located in certs folder and has SERIALNUMBER=tp
+
+		// Initialize the vagvalsinfo that is supposed to be in Tjanstekatalogen
+		// when the call
+		// to the virtual service is made
+		// Note certificate serial number is used as sender
+		SokVagvalsInfoMockInput svimi = new SokVagvalsInfoMockInput();
+		List<VagvalMockInputRecord> vagvalInputs = new ArrayList<VagvalMockInputRecord>();
+		VagvalMockInputRecord vi = new VagvalMockInputRecord();
+		vi.receiverId = "vp-test-producer";
+		vi.senderId = "tp";
+		vi.rivVersion = "RIVTABP20";
+		vi.serviceNamespace = "urn:skl:tjanst1:rivtabp20";
+		vi.adress = "https://localhost:19000/vardgivare-b/tjanst1";
+		vagvalInputs.add(vi);
+		svimi.setVagvalInputs(vagvalInputs);
+	}
 }
