60f6f2e5516cbd4b216349516fc69eb0474cba6b
Henrik Augustsson <henrik.augustsson@nordicmedtest.se>
2022-03-30 10:52:39 +0200

    formatting cleanup.

--- vp-services-camel/src/main/java/se/skl/tp/vp/errorhandling/HandleProducerExceptionProcessor.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/errorhandling/HandleProducerExceptionProcessor.java
@@ -14,59 +14,59 @@ import se.skl.tp.vp.exceptions.VpSemanticErrorCodeEnum;
 @Log4j2
 public class HandleProducerExceptionProcessor implements Processor {
 
-  private ExceptionUtil exceptionUtil;
-  private static final String SOAP_XMLNS = "http://schemas.xmlsoap.org/soap/envelope/";
-  private static final Integer HTTP_STATUS_500 = 500;
+    private ExceptionUtil exceptionUtil;
+    private static final String SOAP_XMLNS = "http://schemas.xmlsoap.org/soap/envelope/";
+    private static final Integer HTTP_STATUS_500 = 500;
 
-  @Autowired
-  public HandleProducerExceptionProcessor(ExceptionUtil exceptionUtil) {
-    this.exceptionUtil = exceptionUtil;
-  }
+    @Autowired
+    public HandleProducerExceptionProcessor(ExceptionUtil exceptionUtil) {
+        this.exceptionUtil = exceptionUtil;
+    }
 
-  @Override
-  public void process(Exchange exchange) throws Exception {
-    try {
-      Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
-      if (exception != null) {
-         if (exception instanceof NettyHttpOperationFailedException) {
-               NettyHttpOperationFailedException operationFailedException = (NettyHttpOperationFailedException) exception;
-              if (operationFailedException.getStatusCode() == HTTP_STATUS_500 && operationFailedException.getContentAsString().contains(SOAP_XMLNS)) {
-                  return;
-              }
-          }
-        String messageString = exception.getMessage();
-        if (exception instanceof ReadTimeoutException) {
-          messageString = "Timeout when waiting on response from producer.";
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        try {
+            Exception exception = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+            if (exception != null) {
+                if (exception instanceof NettyHttpOperationFailedException) {
+                    NettyHttpOperationFailedException operationFailedException = (NettyHttpOperationFailedException) exception;
+                    if (operationFailedException.getStatusCode() == HTTP_STATUS_500 && operationFailedException.getContentAsString().contains(SOAP_XMLNS)) {
+                        return;
+                    }
+                }
+                String messageString = exception.getMessage();
+                if (exception instanceof ReadTimeoutException) {
+                    messageString = "Timeout when waiting on response from producer.";
+                }
+
+                log.debug("Exception Caught by Camel when contacting producer. Exception information: " + left(messageString, 200) + "...");
+
+
+                VpSemanticErrorCodeEnum errorCode = VpSemanticErrorCodeEnum.getDefault();
+                String message = exceptionUtil.createMessage(errorCode);
+
+                String addr = (String) exchange.getProperty(VPExchangeProperties.VAGVAL, "<UNKNOWN>");
+                String vpMsg = String.format("%s. Exception Caught by Camel when contacting producer. Exception information: (%s: %s)",
+                        addr, exception.getClass().getName(), messageString);
+                String messageDetails = exceptionUtil.createDetailsMessage(errorCode, vpMsg);
+
+                SoapFaultHelper.setSoapFaultInResponse(exchange, message, messageDetails, VpSemanticErrorCodeEnum.getDefault());
+            }
+        } catch (Exception e) {
+            log.error("An error occured in HandleProducerExceptionProcessor", e);
+            throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.getDefault(), "unknown");
         }
 
-        log.debug("Exception Caught by Camel when contacting producer. Exception information: " + left(messageString, 200) + "...");
-
-
-        VpSemanticErrorCodeEnum errorCode = VpSemanticErrorCodeEnum.getDefault();
-        String message = exceptionUtil.createMessage(errorCode);
-
-        String addr = (String) exchange.getProperty(VPExchangeProperties.VAGVAL, "<UNKNOWN>");
-        String vpMsg = String.format("%s. Exception Caught by Camel when contacting producer. Exception information: (%s: %s)",
-            addr, exception.getClass().getName(), messageString);
-        String messageDetails = exceptionUtil.createDetailsMessage(errorCode, vpMsg);
-
-        SoapFaultHelper.setSoapFaultInResponse(exchange, message, messageDetails, VpSemanticErrorCodeEnum.getDefault());
-      }
-    } catch (Exception e) {
-      log.error("An error occured in HandleProducerExceptionProcessor", e);
-      throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.getDefault(), "unknown");
     }
 
-  }
+    private String left(String s, int len) {
+        if (s == null) {
+            return null;
+        }
 
-  private String left(String s, int len) {
-    if (s == null) {
-      return null;
+        int i = s.length() > len ? len : s.length();
+        return s.substring(0, i);
     }
-
-    int i = s.length() > len ? len : s.length();
-    return s.substring(0, i);
-  }
 
 
 }
