0f589ae026f75fce1ca20d2376c3a7c5a4a004b9
Jonas Mattsson <jonas.mattsson@nordicmedtest.se>
2023-10-29 00:50:07 +0200

    added test

--- vp-services-camel/src/test/java/se/skl/tp/vp/logging/LogMessageFormatterTest.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/logging/LogMessageFormatterTest.java
@@ -1,17 +1,28 @@
 package se.skl.tp.vp.logging;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import se.skl.tp.vp.constants.VPExchangeProperties;
 import se.skl.tp.vp.logging.logentry.LogEntry;
 import se.skl.tp.vp.logging.logentry.LogMessageType;
 import se.skl.tp.vp.logging.logentry.LogMetadataInfoType;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.util.AssertionErrors.assertNull;
 import static org.wildfly.common.Assert.assertNotNull;
 
@@ -42,5 +53,42 @@ public class LogMessageFormatterTest {
         LogMessageFormatter.format(logEventType, logEntry, messageMap);
         assertEquals(payload, messageMap.get("payload"));
 
+    }
+
+    @Test
+    public void testObjectMessageLog() throws Exception {
+        Logger logger = mock(Logger.class);
+        Exchange exchange = mock(Exchange.class);
+        Message in = mock(Message.class);
+        CamelContext ctx = mock(CamelContext.class);
+
+        Mockito.when(exchange.getProperty(VPExchangeProperties.EXCHANGE_CREATED, Date.class)).thenReturn(new Date());
+        Mockito.when(exchange.getProperty(anyString(), eq(String.class))).thenReturn("mockedString");
+        Mockito.when(exchange.getMessage()).thenReturn(in);
+        Mockito.when(exchange.getIn()).thenReturn(in);
+        Mockito.when(exchange.getContext()).thenReturn(ctx);
+        Mockito.when(logger.isDebugEnabled()).thenReturn(false);
+
+        MessageInfoLogger mil = new MessageInfoLogger();
+        Assertions.assertThrows(Exception.class, () -> {
+            mil.objLog(null, exchange, null);
+        });
+        mil.objLog(logger, null, "testObjectLogger");
+        Logger logger2 = mock(Logger.class);
+        Mockito.when(logger2.isDebugEnabled()).thenReturn(true);
+        mil.objLog(logger2, exchange, "testObjectLogger");
+        assertNotNull(logger2);
+
+        LogEntry logEntry = new LogEntry();
+        logEntry.setPayload(exchange.getIn().getBody(String.class));
+        Map<String, Object> msgMap = new HashMap<>();
+        logEntry.setMessageInfo(new LogMessageType());
+        LogMessageFormatter.format("logEvent-test", logEntry, msgMap);
+
+        assertNotNull(msgMap);
+        assertEquals("logEvent-test", msgMap.get("logEventType"));
+        LogMessageType lmt = (LogMessageType) msgMap.get("messageInfo");
+        assertNull("messageinfo.message should be null", lmt.getMessage());
+        assertNull("messageinfo.exception should be null", lmt.getException());
     }
 }
