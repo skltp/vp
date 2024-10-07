16fbcc3e35272a18390445f4511b41dadab94d4a
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2024-10-07 09:52:04 +0200

    ResetHsaCache test fix

--- vp-services-camel/src/test/java/se/skl/tp/vp/vagval/ResetHsaCacheProcessorTest.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/vagval/ResetHsaCacheProcessorTest.java
@@ -1,22 +1,21 @@
 package se.skl.tp.vp.vagval;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isA;
+import static se.skl.tp.vp.util.JunitUtil.assertStringContains;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.support.DefaultExchange;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import se.skl.tp.hsa.cache.HsaCache;
 import se.skl.tp.hsa.cache.HsaCacheInitializationException;
-import static se.skl.tp.vp.util.JunitUtil.assertStringContains;
-
 
 @CamelSpringBootTest
 @SpringBootTest(classes = VagvalTestConfiguration.class)
@@ -24,37 +23,37 @@ public class ResetHsaCacheProcessorTest {
     @Autowired
     ResetHsaCacheProcessor processor;
 
-    @MockBean(name="hsaCache")
+    @MockBean(name="hsaCacheImpl")
     HsaCache hsaCacheMock;
 
     @BeforeEach
     public void beforeTest() {
-        Mockito.when(hsaCacheMock.init(any())).thenReturn(hsaCacheMock);
+        Mockito.when(hsaCacheMock.init(isA(String[].class))).thenReturn(hsaCacheMock);
     }
 
-//    @Test
-//    public void testResetIsOK() throws Exception {
-//        Exchange ex = createExchange();
-//        Mockito.when(hsaCacheMock.getHSACacheSize()).thenReturn(5).thenReturn(10);
-//        processor.process(ex);
-//        assertStringContains(ex.getMessage().getBody(String.class), "Successfully reset HSA cache");
-//    }
-//
-//    @Test
-//    public void testResetWarning() throws Exception {
-//        Exchange ex = createExchange();
-//        Mockito.when(hsaCacheMock.getHSACacheSize()).thenReturn(10).thenReturn(1);
-//        processor.process(ex);
-//        assertStringContains(ex.getMessage().getBody(String.class), "Warning: HSA cache reset to");
-//    }
-//
-//    @Test
-//    public void testResetException() throws Exception {
-//        Exchange ex = createExchange();
-//        Mockito.when(hsaCacheMock.init(any())).thenThrow(HsaCacheInitializationException.class);
-//        processor.process(ex);
-//        assertStringContains(ex.getMessage().getBody(String.class), "Reset HSA cache failed.");
-//    }
+    @Test
+    public void testResetIsOK() throws Exception {
+        Exchange ex = createExchange();
+        Mockito.when(hsaCacheMock.getHSACacheSize()).thenReturn(5).thenReturn(10);
+        processor.process(ex);
+        assertStringContains(ex.getMessage().getBody(String.class), "Successfully reset HSA cache");
+    }
+
+    @Test
+    public void testResetWarning() throws Exception {
+        Exchange ex = createExchange();
+        Mockito.when(hsaCacheMock.getHSACacheSize()).thenReturn(10).thenReturn(1);
+        processor.process(ex);
+        assertStringContains(ex.getMessage().getBody(String.class), "Warning: HSA cache reset to");
+    }
+
+    @Test
+    public void testResetException() throws Exception {
+        Exchange ex = createExchange();
+        Mockito.when(hsaCacheMock.init(isA(String[].class))).thenThrow(HsaCacheInitializationException.class);
+        processor.process(ex);
+        assertStringContains(ex.getMessage().getBody(String.class), "Reset HSA cache failed.");
+    }
 
 
     private Exchange createExchange() {
