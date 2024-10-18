e8e2a5a5d708e43ecd0c62b7c8126355d5513fd4
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2024-10-18 14:41:29 +0200

    Removed redundant streamCaching (now default)

--- vp-services-camel/src/main/java/se/skl/tp/vp/VPRouter.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/VPRouter.java
@@ -167,7 +167,6 @@ public class VPRouter extends RouteBuilder {
             .end();
 
         from(DIRECT_VP).routeId(VAGVAL_ROUTE)
-            .streamCaching()
             .setProperty(VPExchangeProperties.HTTP_URL_IN,  header(Exchange.HTTP_URL))
             .setProperty(VPExchangeProperties.VP_X_FORWARDED_HOST,  header("{{http.forwarded.header.host}}"))
             .setProperty(VPExchangeProperties.VP_X_FORWARDED_PORT,  header("{{http.forwarded.header.port}}"))
